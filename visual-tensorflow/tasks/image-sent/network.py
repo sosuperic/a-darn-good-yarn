a609eb8976c336d6763f5b3b3006eea1b61a0c63
Eric Chu <eric.chu.56@gmail.com>
2016-12-23 09:21:21 -0800

    Fix / clean up summary ops (separate for train and valid)
    Key things to note:
    1) Two separate FileWriters. Second one (for valid set) doesn't
    pass in graph because it's the same graph
    2) When running training, run summary_op from tf.summary.merge_all()
    - This runs all of the summary ops
    3) When running validation, just run the loss summary and acc summary ops, but
    now with validation data passed in
    
    Note: training step vs valid set is so much slower... and it's not because of the
    backpro step... it's a direct function of the size. Might be a problem with feed_dict.
    Not sure if it's supposed to be used if I'm using queues?
    
    Test plan:
    1) python tasks/image-sent/main.py --arch basic_cnn -obj sent_triclass --mode train --val_every_epoch 1
    2) Go to tensorboard
    - See plots on same graph

--- visual-tensorflow/tasks/image-sent/network.py
+++ visual-tensorflow/tasks/image-sent/network.py
@@ -13,103 +13,80 @@ class Network(object):
         self.params = params
         self.dataset = get_dataset(params)
 
+    ####################################################################################################################
+    # Train
+    ####################################################################################################################
     def train(self):
         """Train"""
-        logs_path = os.path.join(os.path.dirname(__file__), 'logs')
-        _, self.logger = setup_logging(save_path=os.path.join(
-            logs_path, 'train-{}-{}.log'.format(self.params['arch'], self.params['obj'])))
-        # _, self.logger = setup_logging(save_path=logs_path)
-
+        self.logger = self._get_logger()
+        self.output_dim = self.dataset.get_output_dim()
         with tf.Session() as sess:
             # Get model
-            model = None
-            output_dim = self.dataset.get_output_dim()
-            self.logger.info('Making {} model'.format(self.params['arch']))
-            if self.params['arch'] == 'basic_cnn':
-                model = BasicVizsentCNN(batch_size=self.params['batch_size'],
-                                        img_w=self.params['img_crop_w'],
-                                        img_h=self.params['img_crop_h'],
-                                        output_dim=output_dim)
-            elif 'vgg' in self.params['arch']:
-                load_weights = True if self.params['arch'] == 'vgg_finetune' else False
-                model = vgg16(batch_size=self.params['batch_size'],
-                              w=self.params['img_crop_w'],
-                              h=self.params['img_crop_h'],
-                              sess=sess,
-                              load_weights=load_weights,
-                              output_dim=output_dim)
+            model = self._get_model(sess)
 
             # Get data
             self.logger.info('Retrieving training data and setting up graph')
             splits = self.dataset.setup_graph()
             tr_img_batch, tr_label_batch = splits['train']['img_batch'], splits['train']['label_batch']
-            va_img_batch, va_label_batch = splits['train']['img_batch'], splits['train']['label_batch']
+            va_img_batch, va_label_batch = splits['valid']['img_batch'], splits['valid']['label_batch']
 
             # Loss
-            if self.params['obj'] == 'sent_reg':
-                loss = tf.sqrt(tf.reduce_mean(tf.square(tf.sub(model.last_fc, tr_label_batch))))
-            else:
-                labels_onehot = tf.one_hot(tr_label_batch, output_dim)     # (batch_size, num_classes)
-                loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(model.probs, labels_onehot))
-
-                # Accuracy
-                acc = tf.equal(tf.cast(tf.argmax(model.probs, 1), tf.int32), tr_label_batch)
-                acc = tf.reduce_mean(tf.cast(acc, tf.float32))
+            self._get_loss(model)
 
             # Optimize - split into two steps (get gradients and then apply so we can create summary vars)
             optimizer = get_optimizer(self.params['optim'], self.params['lr'])
-            grads = tf.gradients(loss, tf.trainable_variables())
-            grads_and_vars = list(zip(grads, tf.trainable_variables()))
-            train_step = optimizer.apply_gradients(grads_and_vars=grads_and_vars)
+            grads = tf.gradients(self.loss, tf.trainable_variables())
+            self.grads_and_vars = list(zip(grads, tf.trainable_variables()))
+            train_step = optimizer.apply_gradients(grads_and_vars=self.grads_and_vars)
 
-            # Summary ops
-            tf.summary.scalar('loss', loss)
-            if self.params['obj'] != 'sent_reg':    # classification, thus has accuracy
-                tf.summary.scalar('accuracy', acc)
-            for var in tf.trainable_variables():
-                tf.summary.histogram(var.op.name, var)
-            for grad, var in grads_and_vars:
-                tf.summary.histogram(var.op.name+'/gradient', grad)
-            summary_op = tf.summary.merge_all()
+            # Summary ops and writer
+            summary_op = self._get_summary_ops()
             tr_summary_writer = tf.summary.FileWriter(self.params['save_dir'] + '/train', graph=tf.get_default_graph())
-            va_summary_writer = tf.summary.FileWriter(self.params['save_dir'] + '/valid', graph=tf.get_default_graph())
+            va_summary_writer = tf.summary.FileWriter(self.params['save_dir'] + '/valid')
 
             # Initialize after optimization - this needs to be done after adam
-            sess.run(tf.global_variables_initializer())
-
-            coord = tf.train.Coordinator()
-            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+            coord, threads = self._initialize(sess)
 
             # Training
             saver = tf.train.Saver(max_to_keep=None)
             for i in range(self.params['epochs']):
                 self.logger.info('Epoch {}'.format(i))
                 # Normally slice_input_producer should have epoch parameter, but it produces a bug when set. So,
-                num_batches = self.dataset.get_num_batches('train')
-                for j in range(num_batches):
-                    _, loss_val, acc_val, summary = sess.run([train_step, loss, acc, summary_op],
-                                                             feed_dict={'img_batch:0': tr_img_batch.eval()})
-                    self.logger.info('Train minibatch {} / {} -- Loss: {}'.format(j, num_batches, loss_val))
+                num_tr_batches = self.dataset.get_num_batches('train')
+                for j in range(num_tr_batches):
+                    _, loss_val, acc_val, summary = sess.run([train_step, self.loss, self.acc, summary_op],
+                                                             feed_dict={'img_batch:0': tr_img_batch.eval(),
+                                                                        'label_batch:0': tr_label_batch.eval()})
+
+                    self.logger.info('Train minibatch {} / {} -- Loss: {}'.format(j, num_tr_batches, loss_val))
                     self.logger.info('................... -- Acc: {}'.format(acc_val))
 
-                    # Write summary
+                    # # Write summary
                     if j % 10 == 0:
-                        tr_summary_writer.add_summary(summary, i * num_batches + j)
+                        tr_summary_writer.add_summary(summary, i * num_tr_batches + j)
 
-                    # if j > 5:
+                    # if j == 5:
                     #     break
 
                 # Evaluate on validation set (potentially)
-                num_batches = self.dataset.get_num_batches('valid')
-                for j in range(num_batches):
-                    loss_val, acc_val, summary = sess.run([loss, acc, summary_op],
-                                                          feed_dict={'img_batch:0': va_img_batch.eval()})
-                    self.logger.info('Valid minibatch {} / {} -- Loss: {}'.format(j, num_batches, loss_val))
-                    self.logger.info('................... -- Acc: {}'.format(acc_val))
+                if (i+1) % self.params['val_every_epoch'] == 0:
+                    num_va_batches = self.dataset.get_num_batches('valid')
+                    for j in range(num_va_batches):
+                        loss_val, acc_val, loss_summary, acc_summary = sess.run([self.loss, self.acc,
+                                                                    self.loss_summary, self.acc_summary],
+                                                              feed_dict={'img_batch:0': va_img_batch.eval(),
+                                                                        'label_batch:0': va_label_batch.eval()})
 
-                    # Write summary
-                    if j % 10 == 0:
-                        va_summary_writer.add_summary(summary, i * num_batches + j)
+                        self.logger.info('Valid minibatch {} / {} -- Loss: {}'.format(j, num_va_batches, loss_val))
+                        self.logger.info('................... -- Acc: {}'.format(acc_val))
+
+                        # Write summary
+                        if j % 10 == 0:
+                            va_summary_writer.add_summary(loss_summary, i * num_tr_batches + j)
+                            va_summary_writer.add_summary(acc_summary, i * num_tr_batches + j)
+
+                        # if j == 5:
+                        #     break
 
                 # Save model at end of epoch (potentially)
                 save_model(sess, saver, self.params, i, self.logger)
@@ -117,12 +94,114 @@ class Network(object):
             coord.request_stop()
             coord.join(threads)
 
-
+    ####################################################################################################################
+    # Test
+    ####################################################################################################################
     def test(self):
         """Test"""
+        self.logger = self._get_logger()
+        self.output_dim = self.dataset.get_output_dim()
         with tf.Session() as sess:
-            print 'Load model'
-
+            # Get model
+            self.logger.info('Load model')
             saver = load_model(sess, self.params)
-
             # print sess.run(tf.trainable_variables()[0])
+
+            # Get data
+            self.logger.info('Getting test set')
+            splits = self.dataset.setup_graph()
+            te_img_batch, te_label_batch = splits['test']['img_batch'], splits['test']['label_batch']
+
+            # Loss
+            self._get_loss(saver)
+
+            # Summary ops and writer
+            summary_op = self._get_summary_ops()
+            summary_writer = tf.summary.FileWriter(self.params['save_dir'] + '/test', graph=tf.get_default_graph())
+
+            # Initialize
+            coord, threads = self._initialize(sess)
+
+            # Test
+            num_batches = self.dataset.get_num_batches('test')
+            for j in range(num_batches):
+                loss_val, acc_val, summary = sess.run([self.loss, self.acc, summary_op],
+                                                      feed_dict={'img_batch:0': te_img_batch.eval(),
+                                                                 'label_batch:0': te_label_batch.eval()})
+
+                self.logger.info('Test minibatch {} / {} -- Loss: {}'.format(j, num_batches, loss_val))
+                self.logger.info('................... -- Acc: {}'.format(acc_val))
+
+                # Write summary
+                if j % 1 == 0:
+                    summary_writer.add_summary(summary, j)
+
+
+            coord.request_stop()
+            coord.join(threads)
+
+    ####################################################################################################################
+    # Helper functions
+    ####################################################################################################################
+    def _get_logger(self):
+        """Return logger, where path is dependent on mode (train/test), arch, and obj"""
+        logs_path = os.path.join(os.path.dirname(__file__), 'logs')
+        _, logger = setup_logging(save_path=os.path.join(
+            logs_path, '{}-{}-{}.log'.format(self.params['mode'], self.params['arch'], self.params['obj'])))
+        # _, self.logger = setup_logging(save_path=logs_path)
+        return logger
+
+    def _get_model(self, sess=None):
+        """Return model (sess is required to load weights for vgg)"""
+        # Get model
+        model = None
+        self.logger.info('Making {} model'.format(self.params['arch']))
+        if self.params['arch'] == 'basic_cnn':
+            model = BasicVizsentCNN(batch_size=self.params['batch_size'],
+                                    img_w=self.params['img_crop_w'],
+                                    img_h=self.params['img_crop_h'],
+                                    output_dim=self.output_dim)
+        elif 'vgg' in self.params['arch']:
+            load_weights = True if self.params['arch'] == 'vgg_finetune' else False
+            model = vgg16(batch_size=self.params['batch_size'],
+                          w=self.params['img_crop_w'],
+                          h=self.params['img_crop_h'],
+                          sess=sess,
+                          load_weights=load_weights,
+                          output_dim=self.output_dim)
+        return model
+
+    def _get_loss(self, model):
+        if self.params['obj'] == 'sent_reg':
+            label_batch_op = tf.placeholder(tf.float32, shape=[self.params['batch_size']],
+                                            name='label_batch')
+            self.loss = tf.sqrt(tf.reduce_mean(tf.square(tf.sub(model.last_fc, label_batch_op))))
+        else:
+            label_batch_op = tf.placeholder(tf.int32, shape=[self.params['batch_size']],
+                                            name='label_batch')
+            labels_onehot = tf.one_hot(label_batch_op, self.output_dim)     # (batch_size, num_classes)
+            self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(model.probs, labels_onehot))
+
+            # Accuracy
+            acc = tf.equal(tf.cast(tf.argmax(model.probs, 1), tf.int32), label_batch_op)
+            self.acc = tf.reduce_mean(tf.cast(acc, tf.float32))
+
+    def _get_summary_ops(self):
+        """Define summaries and return summary_op"""
+        self.loss_summary = tf.summary.scalar('loss', self.loss)
+        if self.params['obj'] != 'sent_reg':    # classification, thus has accuracy
+            self.acc_summary = tf.summary.scalar('accuracy', self.acc)
+
+        # Weights and gradients
+        for var in tf.trainable_variables():
+            tf.summary.histogram(var.op.name, var)
+        for grad, var in self.grads_and_vars:
+            tf.summary.histogram(var.op.name+'/gradient', grad)
+        summary_op = tf.summary.merge_all()
+        return summary_op
+
+    def _initialize(self, sess):
+        sess.run(tf.global_variables_initializer())
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        return coord, threads
