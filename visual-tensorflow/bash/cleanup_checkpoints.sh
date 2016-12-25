59e4a06ba1a0db8483746f770e3dbbd64ace9bfa
Eric Chu <eric.chu.56@gmail.com>
2016-12-24 22:02:17 -0800

    [Cleanup checkpoints script] Remove if no checkpoints

--- visual-tensorflow/bash/cleanup_checkpoints.sh
+++ visual-tensorflow/bash/cleanup_checkpoints.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# If checkpoint directory only has params.json, either (a) an error was thrown, or (b) I terminated it early.
+# If checkpoint directory doesn't have checkpoint, either (a) an error was thrown, or (b) I terminated it early.
 # Keeping these around just clutters things
 
 cd tasks
@@ -8,10 +8,8 @@ for task_dir in */ ; do
     cd $task_dir/checkpoints
     for ckpt_dir in */ ; do
         if [ -d ${ckpt_dir} ]; then        # will not run if no directories are available
-            if [ $(ls $ckpt_dir | wc -l) == 1 ]; then
-                if [ $(ls $ckpt_dir) == "params.json" ]; then
-                    rm -rf $ckpt_dir
-                fi
+            if [ -e "checkpoint" ]; then
+                rm -rf $ckpt_dir
             fi
         fi
     done
