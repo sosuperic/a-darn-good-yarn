e4efe94942e385b4fa945d76dffec93d914d6846
Eric Chu <eric.chu.56@gmail.com>
2016-12-29 14:37:13 -0800

    [Website][Stable] Link dat.gui with recalculating preds and redrawing
    General overview:
    1) In controller (views.py), visiting template /shape does 2 things:
    (a) return data with (relative) framepaths, titles of videos, and
    predictions for one movie
    (b) sets global variable within views.py that keeps track of
    current video and current dataframe created from csv predictions file
    
    2) When dat.gui parameters are changed, make ajax call to retrieve new predictions
    if windowSize or video has changed
    - This sets the global vars within views.py
    - Returned preds are set in plot_shape.html
    - Call draw_graph, which uses new preds
    
    Test plan:
    1) Have sent_biclass predictions for Feast and Frozen
    2) http://localhost:7898/shape
    - Be able to toggle between the two, alter dat gui parameters

--- visual-tensorflow/website/shape/views.py
+++ visual-tensorflow/website/shape/views.py
@@ -13,7 +13,7 @@ import random
 import string
 import re
 import os
-import numpy
+import numpy as np
 import pandas as pd
 
 
@@ -21,51 +21,116 @@ from natsort import natsorted
 
 VIDEOS_PATH = 'data/videos/'
 
-feast_path = os.path.join(VIDEOS_PATH, '@Animated/@NewDisney/Feast')
-frames = natsorted([f for f in os.listdir(os.path.join(feast_path, 'frames')) if not f.startswith('.')])
-frames = [os.path.join('@Animated/@NewDisney/Feast', 'frames', f) for f in frames]
-pred = os.path.join('@Animated/@NewDisney/Feast', 'preds', 'sent_biclass_0.csv')
+title2vidpath = {}
+cur_pd_df = None
+cur_title = None
 
-
-def smooth(x,window_len=11,window='hanning'):
+#################################################################################################
+# NON-ROUTING FUNCTIONS 
+#################################################################################################
+def smooth(x, window_len=48, window='hanning'):
     if x.ndim != 1:
-            raise ValueError, "smooth only accepts 1 dimension arrays."
+        raise ValueError, "smooth only accepts 1 dimension arrays."
     if x.size < window_len:
-            raise ValueError, "Input vector needs to be bigger than window size."
-    if window_len<3:
-            return x
+        raise ValueError, "Input vector needs to be bigger than window size."
+    if window_len < 3:
+        return x
     if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
-            raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
-    s=numpy.r_[2*x[0]-x[window_len-1::-1],x,2*x[-1]-x[-1:-window_len:-1]]
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+    s = np.r_[2 * x[0] - x[window_len-1::-1], x, 2 * x[-1] - x[-1:-window_len:-1]]
     if window == 'flat': #moving average
-            w=numpy.ones(window_len,'d')
+        w = np.ones(window_len,'d')
     else:  
-            w=eval('numpy.'+window+'(window_len)')
-    y=numpy.convolve(w/w.sum(),s,mode='same')
+        w=eval('np.' + window + '(window_len)')
+    y = np.convolve(w/w.sum(), s, mode='same')
     return y[window_len:-window_len+1]
 
 
-vid = pd.read_csv('data/videos/@Animated/@NewDisney/Feast/preds/sent_biclass_0.csv')
-smoothed = smooth(vid.pos.values, window_len=int(48))
+def get_preds_from_df(df, window_len=48):
+    """Return smoothed preds: each key is the label, value is list of frame-wise predictions"""
+    if len(df.columns) == 2:     # sent_biclass -> just show positive
+        preds = {'pos': list(smooth(df.pos.values, window_len=window_len))}
+    else:
+        preds = {}
+        for c in df.columns:
+            preds[c] = list(smooth(df[c].values, window_len=window_len))
+    return preds
 
-data = {'frames': frames, 'pred': pred, 'smoothed': list(smoothed)}
+#################################################################################################
+# INITIAL SETUP
+#################################################################################################
+def get_all_vidpaths_with_preds():
+    """
+    Return list of full paths to every video directory that contains predictions
+    e.g. [<VIDEOS_PATH>/@Animated/@OldDisney/Feast/, ...]
+    """
+    vidpaths = []
+    for root, dirs, files in os.walk(VIDEOS_PATH):
+        if ('frames' in os.listdir(root)) and ('preds' in os.listdir(root)):
+            vidpaths.append(root)
+    return vidpaths
 
+def get_initial_data():
+    """
+    Return initial data to pass to template. 
+    Also set global variables cur_pd_df and cur_title
+    """
+    vidpaths = get_all_vidpaths_with_preds()
+    titles = []
+    global title2vidpath
+    for vp in vidpaths:
+        t = os.path.basename(vp)
+        titles.append(t)
+        title2vidpath[t] = vp
+    print title2vidpath
 
-@app.route('/api/pred/<window>', methods=['GET'])
-def get_preds(window):
-    vid = pd.read_csv('data/videos/@Animated/@NewDisney/Feast/preds_old_torch/posneg.csv',
-                  header=None, names=['Positive', 'Negative'])
-    smoothed = smooth(vid.Positive.values, window_len=int(window))
+    # Get paths every frame for every video
+    title2framepaths = {}
+    for i, t in enumerate(titles):
+        vp = vidpaths[i]
+        title2framepaths[t] = natsorted([os.path.join(vp, 'frames', f) for f in os.listdir(os.path.join(vp, 'frames')) \
+        if not f.startswith('.')])
+
+    # Get initial preds (first video to show)
+    global cur_pd_df
+    cur_pd_df = pd.read_csv(os.path.join(vidpaths[0], 'preds', 'sent_biclass.csv'))
+    preds = get_preds_from_df(cur_pd_df, window_len=48)
+    
+    # Now turn full paths to relative paths because js has relative path starting from videos/
+    vidpaths = [vp.lstrip(VIDEOS_PATH) for vp in vidpaths]
+    title2framepaths = {t: [fp.lstrip(VIDEOS_PATH) for fp in vid] for t, vid in title2framepaths.items()}
+
+    global cur_title
+    cur_title = titles[0]
+
+    data = {'titles': titles, 'title2framepaths': title2framepaths, 'preds': preds}
+    return data
+
+#################################################################################################
+# ROUTING FUNCTIONS 
+#################################################################################################
+@app.route('/shape', methods=['GET'])
+def shape():
+    """Main shape template with initial data"""
+    data = get_initial_data()
+    return render_template('plot_shape.html', data=json.dumps(data))
+
+@app.route('/api/pred/<title>/<window_len>', methods=['GET'])
+def get_preds(title, window_len):
+    """Return predictions for a given movie with window_len, update global vars"""
+    global cur_pd_df, cur_title, title2vidpath
+    if title != cur_title:
+        cur_title = title
+        vidpath = title2vidpath[title]
+        cur_pd_df = pd.read_csv(os.path.join(vidpath, 'preds', 'sent_biclass.csv'))
+
+    preds = get_preds_from_df(cur_pd_df, window_len=int(window_len))
 
     return Response(
-        json.dumps(list(smoothed)),
+        json.dumps(preds),
         mimetype='application/json',
         headers={
             'Cache-Control': 'no-cache',
             'Access-Control-Allow-Origin': '*'
         }
     )
-
-@app.route('/shape', methods=['GET'])
-def shape():
-    return render_template('plot_shape.html', data=json.dumps(data))
