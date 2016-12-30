0920c0c63d358ab851b77eaf7ce42c5b2719af07
Eric Chu <eric.chu.56@gmail.com>
2016-12-29 22:17:29 -0800

    [Website] Change some things to run on Shannon
    1) Instead of getting all framepaths for every video, just get
    framepaths for initial video. This step was slow on Shannon
    when there are hundreds of folders and thousands of frames for every
    video
    2) Since website/ already has symlink to videos in order to show images
    (see earlier commit), just use that as VIDEOS_PATH in views.py
    3) Have an initial setup function
    
    NOTE: Compared to local, where displaying images is very responsive,
    and altering parameters using dat.gui is quick, Shannon is much slower...
    Maybe should push somethings to the cloud? Preload images? Idk.
    
    To open up port, followed my old notes in:
    https://github.mit.edu/lsm/electome-dashboard-orig
    
    Test plan:
    1) Run locally, in website/
    `python run.py`
    2) Runo n shannon, in website/
    `gunicorn -w 10 -b 0.0.0.0:7898 run:app`
    - Hundreds of movies available (see recent earlier commit to
    predict over all movies)

--- visual-tensorflow/website/run.py
+++ visual-tensorflow/website/run.py
@@ -2,4 +2,7 @@
 import os
 
 from shape import app
-app.run(port=int(os.environ.get("PORT", 7898)), debug=True)
+
+if __name__ == '__main__':
+    app.run(port=int(os.environ.get("PORT", 7898)), debug=True)
+    # app.run(host='0.0.0.0', port=7898)
--- visual-tensorflow/website/shape/views.py
+++ visual-tensorflow/website/shape/views.py
@@ -1,34 +1,25 @@
 # Controller for viewing shape 
 
+
 import json
+from natsort import natsorted
+import numpy as np
 import os
-import time
+import pandas as pd
 from flask import Flask, Response, request, render_template
+
 from shape import app
 
-import uuid
-import csv
-import pickle
-import random
-import string
-import re
-import os
-import numpy as np
-import pandas as pd
-
-
-from natsort import natsorted
-
-VIDEOS_PATH = 'data/videos/'
+VIDEOS_PATH = 'shape/static/videos/'
 
 title2vidpath = {}
-title2framepaths = {}
+titles = []
 cur_pd_df = None
 cur_title = None
 cur_vid_framepaths = None
 
 #################################################################################################
-# NON-ROUTING FUNCTIONS 
+# PREDICTION FUNCTIONS 
 #################################################################################################
 def smooth(x, window_len=48, window='hanning'):
     if x.ndim != 1:
@@ -72,41 +63,41 @@ def get_all_vidpaths_with_preds():
             vidpaths.append(root)
     return vidpaths
 
-def get_initial_data():
+def get_cur_vid_framepaths():
+    global cur_vid_framepaths
+    cur_vid_framepaths = [f for f in os.listdir(os.path.join(title2vidpath[cur_title], 'frames')) if \
+            not f.startswith('.')]
+    cur_vid_framepaths = [os.path.join(title2vidpath[cur_title].lstrip(VIDEOS_PATH), 'frames', f) for \
+        f in cur_vid_framepaths]
+    cur_vid_framepaths = natsorted(cur_vid_framepaths)
+    return cur_vid_framepaths
+
+def setup_initial_data():
     """
     Return initial data to pass to template. 
     Also set global variables cur_pd_df and cur_title
     """
-    global title2vidpath, title2framepaths, cur_title, cur_pd_df, cur_vid_framepaths
+    global title2vidpath, titles, cur_title, cur_pd_df, cur_vid_framepaths
 
     vidpaths = get_all_vidpaths_with_preds()
-    titles = []
     for vp in vidpaths:
         t = os.path.basename(vp)
         titles.append(t)
         title2vidpath[t] = vp
+    titles = sorted(titles)
+    print titles
     cur_title = titles[0]
-
-    # Get paths every frame for every video
-    for i, t in enumerate(titles):
-        vp = vidpaths[i]
-        title2framepaths[t] = natsorted([os.path.join(vp, 'frames', f) for f in os.listdir(os.path.join(vp, 'frames')) \
-        if not f.startswith('.')])
 
     # Get initial preds (first video to show)
     cur_pd_df = pd.read_csv(os.path.join(vidpaths[0], 'preds', 'sent_biclass.csv'))
     cur_vid_preds = get_preds_from_df(cur_pd_df, window_len=48)
     
     # Now turn full paths to relative paths because js has relative path starting from videos/
-    vidpaths = [vp.lstrip(VIDEOS_PATH) for vp in vidpaths]
-    title2framepaths = {t: [fp.lstrip(VIDEOS_PATH) for fp in vid] for t, vid in title2framepaths.items()}
-    cur_vid_framepaths = title2framepaths[cur_title]
+    # vidpaths = [vp.lstrip(VIDEOS_PATH) for vp in vidpaths]
+    # title2framepaths = {t: [fp.lstrip(VIDEOS_PATH) for fp in vid] for t, vid in title2framepaths.items()}
+    cur_vid_framepaths = get_cur_vid_framepaths()
 
-    # titles to create dropdown in dat.gui
-    # framepaths to display image for current video
-    # preds to create graph for current video
-    data = {'titles': titles, 'framepaths': cur_vid_framepaths, 'preds': cur_vid_preds}
-    return data
+    print 'done'
 
 #################################################################################################
 # ROUTING FUNCTIONS 
@@ -114,18 +105,23 @@ def get_initial_data():
 @app.route('/shape', methods=['GET'])
 def shape():
     """Main shape template with initial data"""
-    data = get_initial_data()
+    global titles, cur_pd_df, cur_vid_framepaths
+    cur_vid_preds = get_preds_from_df(cur_pd_df, window_len=48)
+    # titles to create dropdown in dat.gui
+    # framepaths to display image for current video
+    # preds to create graph for current video
+    data = {'titles': titles, 'framepaths': cur_vid_framepaths, 'preds': cur_vid_preds}
     return render_template('plot_shape.html', data=json.dumps(data))
 
 @app.route('/api/pred/<title>/<window_len>', methods=['GET'])
 def get_preds_and_frames(title, window_len):
     """Return predictions for a given movie with window_len, update global vars"""
-    global title2vidpath, title2framepaths, cur_pd_df, cur_title, cur_vid_framepaths
+    global title2vidpath, cur_pd_df, cur_title, cur_vid_framepaths
     if title != cur_title:
         cur_title = title
         vidpath = title2vidpath[title]
         cur_pd_df = pd.read_csv(os.path.join(vidpath, 'preds', 'sent_biclass.csv'))
-        cur_vid_framepaths = title2framepaths[title]
+        cur_vid_framepaths = get_cur_vid_framepaths()
 
     preds = get_preds_from_df(cur_pd_df, window_len=int(window_len))
     data = {'preds': preds, 'framepaths': cur_vid_framepaths}
@@ -138,3 +134,5 @@ def get_preds_and_frames(title, window_len):
             'Access-Control-Allow-Origin': '*'
         }
     )
+
+setup_initial_data()
