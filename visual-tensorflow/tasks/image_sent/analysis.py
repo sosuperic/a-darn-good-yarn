d2039ca9cab74cf98b1ad7f610b048fbd8e562f7
Eric Chu <eric.chu.56@gmail.com>
2017-01-16 13:09:32 -0500

    [Cluster] Ignore credits; Save outputs
    - If 'credits_index.txt' exists, get the frame index to ignore
    end of predictions dataframe
    - Command line arguments
    
    Save 5 things:
    Data:
    1) ts (time series - np array (num_series, max_len))
    2) ts_index2title (time series index to the title so we can display in GUI)
    Clusters:
    3) assignments, e.g. {0: [1,11, 20,], ...}
    4) centroids, e.g. array for each cluster centroid
    5) Image of cluster
    
    Test plan:
    1) Local:
    - python tasks/image_sent/analysis.py --root_videos_dir data/videos/films --pred_fn sent_biclass.csv -w 100 -k 1,2
    - See figures and saved outputs
    2) Shannon:
    - python tasks/image_sent/analysis.py --root_videos_dir data/videos/films -w 1000 -ds 3 --pred_fn sent_biclass_19.csv -k 1,2,3,4,5,6,7,8,9,10

--- visual-tensorflow/tasks/image_sent/analysis.py
+++ visual-tensorflow/tasks/image_sent/analysis.py
@@ -1,6 +1,10 @@
 # Analyze outputs, e.g. cluster shapes
 
+# TODO: calculate k-means score
+# TODO: what to do about shorts, should we filter by length, idk
+
 import argparse
+from natsort import natsorted
 import os
 import pandas as pd
 import pickle
@@ -12,108 +16,215 @@ from core.predictions.hierarchical_cluster import *
 from core.predictions.utils import smooth
 from core.utils.utils import setup_logging
 
+OUTPUTS_PATH = 'outputs/cluster/'
+VIDEOS_PATH = 'data/videos'
 
 class Analysis(object):
-    def __init__(self, method):
-        self.method = method
+    def __init__(self):
         self.logger = self._get_logger()
 
-    def cluster_timeseries(self, data):
-        """data is np array of [num_timeseries, max_len]"""
-        if self.method == 'dtw':
-            self.cluster_timeseries_dtw(data)
-        elif self.method == 'hierarchical':
+    ####################################################################################################################
+    # Cluster
+    ####################################################################################################################
+    def cluster_timeseries(self, data, method, k):
+        """
+        Cluster data and save outputs
+
+        Parameters
+        ----------
+        data: np of dimension [num_timeseries, max_len]
+        method: str, clustering method to use
+        k: number of clusters (for parametric clustering techniques)
+        """
+        if method == 'dtw':
+            self.cluster_timeseries_dtw(data, k)
+        elif method == 'hierarchical':
             self.cluster_timeseries_hierarchical(data)
         else:
-            self.logger.info('Method unknown: {}'.format(self.method))
+            self.logger.info('Method unknown: {}'.format(method))
 
         self.logger.info('Done clustering')
 
     def cluster_timeseries_hierarchical(self, data):
-        """data is np array of [num_timeseries, max_len]"""
+        """
+        Parameters
+        ----------
+        data: np of dimension [num_timeseries, max_len]
+        """
         clusterer = HierarchicalCluster()
         clusterer.cluster(data)
 
-    def cluster_timeseries_dtw(self, data):
-        """data is np array of [num_timeseries, max_len]"""
-        clusterer = ts_cluster(num_clust=4)
-        # clusterer = ts_cluster(num_clust=10)
+    def cluster_timeseries_dtw(self, data, k=4):
+        """
+        Parameters
+        ----------
+        data: np of dimension [num_timeseries, max_len]
+        method: str, clustering method to use
+        k: number of clusters (for parametric clustering techniques)
+        """
+        clusterer = ts_cluster(num_clust=k)
 
-        self.logger.info('K-means clustering')
+        self.logger.info('K-means clustering: k={}'.format(k))
         clusterer.k_means_clust(data, 1, 2)     # num_iter, window_size
 
-        print clusterer.assignments
+        # print clusterer.assignments
         # print clusterer.centroids
 
-        with open('centroids_tmp20-4.pkl', 'w') as f:
+        # Save outputs
+        self.logger.info('Saving centroids, assignments, figure')
+        params_str = 'dir{}-n{}-k{}-w{}-ds{}-fn{}'.format(
+            os.path.basename(self.root_videos_dirpath),
+            self.n, k, self.window_size, self.downsample_rate,
+            self.pred_fn[:-4])      # remove .csv
+        with open(os.path.join(OUTPUTS_PATH, 'data', 'centroids_{}.pkl'.format(params_str)), 'w') as f:
             pickle.dump(clusterer.centroids, f, protocol=2)
-
-        with open('assignments_tmp20-4.pkl', 'w') as f:
+        with open(os.path.join(OUTPUTS_PATH, 'data', 'assignments_{}.pkl'.format(params_str)), 'w') as f:
             pickle.dump(clusterer.assignments, f, protocol=2)
+        # Plots
+        for c in clusterer.centroids:
+            plt.plot(c)
+        plt.savefig(os.path.join(OUTPUTS_PATH, 'imgs', 'dtw_{}.png'.format(params_str)))
+
+        for centroid_idx, assignments in clusterer.assignments.items():
+            self.logger.info('Centroid {}: {} series'.format(centroid_idx, len(assignments)))
 
         # clusterer.plot_centroids()
 
-    def prepare_timeseries(self):
-        """Create np array of [num_timeseries, max_len]"""
+    ####################################################################################################################
+    # Preprocess data
+    ####################################################################################################################
+
+    def get_credits_idx(self, vid_dirpath):
+        """
+        Return index of frame file that credits begins, if it exists
+
+        Parameters
+        ----------
+        vid_dirpath: path to directory with video
+
+        """
+        if os.path.exists(os.path.join(vid_dirpath, 'credits_index.txt')):
+            with open(os.path.join(vid_dirpath, 'credits_index.txt'), 'r') as f:
+                fn = f.readline().strip('\n')
+            frames = natsorted(os.listdir(os.path.join(vid_dirpath, 'frames')))
+            idx = frames.index(fn)
+            # print fn, idx, len(frames)
+            return idx
+        else:
+            return None
+
+    def prepare_timeseries(self, root_videos_dirpath, window_size, pred_fn, downsample_rate):
+        """
+        Create np array of [num_timeseries, max_len]
+
+        Parameters
+        ----------
+        root_videos_dirpath:
+        window_size: int, window size to use for smoothing
+        pred_fn: filename that contains predictions, e.g. sent_biclass_19.csv
+        downsample_rate: int, ratio at which to downsample time series
+            - used to speed up clustering
+            - e.g. 3 = sample every third point
+        """
 
         # Get all series from videos with predictions
-        self.logger.info('Getting prediction series')
-        series = []
-        vidpaths = self.get_all_vidpaths_with_frames('data/videos')
-        # vidpaths = self.get_all_vidpaths_with_frames('data/videos/films')
-        for vp in vidpaths:
-            preds_path = os.path.join(vp, 'preds', 'sent_biclass.csv')
+        self.logger.info('Preparing timeseries')
+        ts = []
+        ts_idx2title = {}
+
+
+        vid_dirpaths = self.get_all_vidpaths_with_frames_and_preds(root_videos_dirpath)
+        i = 0
+
+        self.logger.info('Getting predictions, removing credits preds, smoothing and downsampling')
+        for vid_dirpath in vid_dirpaths:
+            # self.logger.info('Video: {}'.format(vid_dirpath))
+
+            # Get predictions
+            preds_path = os.path.join(vid_dirpath, 'preds', pred_fn)
             vals = pd.read_csv(preds_path).pos.values
-            window_len = 1000
-            if len(vals) < window_len:
-                self.logger.info('{} length less than: {}'.format(vp, window_len))
+
+            # Skip if window size too big
+            if len(vals) < window_size:
+                self.logger.info('{} length less than: {}'.format(vid_dirpath, window_size))
                 continue
-            smoothed = smooth(vals, window_len=window_len)
-            downsampled = smoothed[::3]
-            series.append(downsampled)
 
-        series = series[0:20]
+            # Remove predictions for credits frame
+            credits_idx = self.get_credits_idx(vid_dirpath)
+            if credits_idx:
+                # self.logger.info('Credits index: {}'.format(credits_idx))
+                vals = vals[:credits_idx]
 
-        self.logger.info('{} series'.format(len(series)))
+            # Smooth and downsample
+            smoothed = smooth(vals, window_len=window_size)
+            downsampled = smoothed[::downsample_rate]
+
+            ts.append(downsampled)
+            ts_idx2title[i] = os.path.basename(vid_dirpath)
+            i += 1
+
+        # For debugging - to have quick results
+        # ts = ts[0:20]
+
         self.logger.info('Interpolating series to maximum series length')
         # Make all timeseries the same length by going from 0% of video to 100% of video and interpolating in between
-        max_len = max([len(s) for s in series])
-        for i, s in enumerate(series):
+        max_len = max([len(s) for s in ts])
+        for i, s in enumerate(ts):
             if len(s) == max_len:
                 continue
             else:
                 s_interp = interp.interp1d(np.arange(s.size), s)
                 s_stretch = s_interp(np.linspace(0, s.size-1, max_len))
-                series[i] = s_stretch
+                ts[i] = s_stretch
 
         # Interpolate each series value between min and max value so as not to include magnitude
         # i.e. min value is 0, max value is 1
-        self.logger.info('Normalizing series')
-        for i, s in enumerate(series):
-            series[i] = (series[i] - series[i].min()) / (series[i].max() - series[i].min())
+        self.logger.info('Normalizing range of each series')
+        for i, s in enumerate(ts):
+            ts[i] = (ts[i] - ts[i].min()) / (ts[i].max() - ts[i].min())
 
-        data = np.array(series)  # (num_timeseries, max_len)
+        ts = np.array(ts)  # (num_timeseries, max_len)
 
-        with open('tmp_ts_data.pkl', 'w') as f:
-            pickle.dump(data, f, protocol=2)
+        # Save time series data
+        self.logger.info('Saving time series data')
+        params_str = 'dir{}-n{}-w{}-ds{}-fn{}'.format(
+            os.path.basename(root_videos_dirpath),
+            len(ts), window_size, downsample_rate,
+            pred_fn[:-4])      # remove .csv
+        with open(os.path.join(OUTPUTS_PATH, 'data', 'ts_{}.pkl'.format(params_str)), 'w') as f:
+            pickle.dump(ts, f, protocol=2)
+        with open(os.path.join(OUTPUTS_PATH, 'data', 'ts_idx2title_{}.pkl'.format(params_str)), 'w') as f:
+            pickle.dump(ts_idx2title, f, protocol=2)
 
-        # Uncomment this for hierarchical cluustering
-        # data = pickle.load(open('tmp_ts_data_all.pkl', 'r'))
+        # Save parameters to use in output filename
+        self.root_videos_dirpath = root_videos_dirpath
+        self.n = len(ts)
+        self.window_size = window_size
+        self.downsample_rate = downsample_rate
+        self.pred_fn = pred_fn
 
-        print data.shape
+        self.logger.info('Number of time series: {}'.format(len(ts)))
+        self.logger.info('Time series length (max): {}'.format(ts.shape[1]))
 
-        return data
+        return ts
 
+    ####################################################################################################################
     # Helper functions
-    def get_all_vidpaths_with_frames(self, starting_dir):
+    ####################################################################################################################
+    def get_all_vidpaths_with_frames_and_preds(self, starting_dir):
         """
-        Return list of full paths to every video directory that contains preds/
-        e.g. [<VIDEOS_PATH>/@Animated/@OldDisney/Feast/, ...]
+        Return list of paths ([<VIDEOS_PATH>/films/M-VAD_full_movies/21 Jump Street/, ...]) to every directory that
+        contains frames/ and preds/
+
+        Parameters
+        ----------
+        starting_dir: e.g. data/videos/films
         """
-        self.logger.info('Getting all vidpaths with frames/')
+        self.logger.info('Traversing: {}'.format(starting_dir))
+        self.logger.info('Finding all directories with frames/ and preds/ folders')
         vidpaths = []
         for root, dirs, files in os.walk(starting_dir):
-            if 'preds' in os.listdir(root):
+            if ('preds' in os.listdir(root)) and ('frames' in os.listdir(root)):
                 vidpaths.append(root)
 
         return vidpaths
@@ -126,9 +237,18 @@ class Analysis(object):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Cluster time series')
+    parser.add_argument('--root_videos_dir', dest='root_videos_dir', default=VIDEOS_PATH,
+                        help='folder to traverse for predictions')
+    parser.add_argument('--pred_fn', dest='pred_fn', default='sent_biclass_19.csv', help='pred file name')
+    parser.add_argument('-w', dest='window_size', type=int, default=1000, help='window size for smoothing predictions')
+    parser.add_argument('-ds', dest='downsample_rate', type=int, default=3, help='downsample rate')
     parser.add_argument('-m', '--method', dest='method', default='dtw', help='dtw,hierarchical')
+    parser.add_argument('-k', dest='k', default='4', help='list of comma-separated k to evaluate')
     cmdline = parser.parse_args()
 
-    analysis = Analysis(cmdline.method)
-    data = analysis.prepare_timeseries()
-    analysis.cluster_timeseries(data)
+    analysis = Analysis()
+    ts = analysis.prepare_timeseries(cmdline.root_videos_dir, cmdline.window_size,
+                                       cmdline.pred_fn, cmdline.downsample_rate)
+    for k in cmdline.k.split(','):
+        print '=' * 100
+        analysis.cluster_timeseries(ts, cmdline.method, int(k))
