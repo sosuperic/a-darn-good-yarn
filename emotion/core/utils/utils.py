7edb63c97d8d6abf57a03f4dae2cb79618f1560d
Eric Chu <eric.chu.56@gmail.com>
2017-02-22 08:26:54 -0500

    [Audio] Use preds from AudioCNN with dropout
    Test plan:
    Want to make sure preds with and without dropout aren't that different
    - First get preds:
      - python tasks/audio_sent/main.py --mode predict --ckpt_dir 2017-02-17___05-17-34 --load_epoch 70 --gpus 3 -bs 1 -obj valence_class -vd data/videos/films/ --stride 5
      - python tasks/audio_sent/main.py --mode predict --ckpt_dir 2017-02-08___08-53-28 --load_epoch 69 --gpus 3 -bs 1 -obj valence_class -vd data/videos/films/ --stride 5
      - (also did it on shorts)
    - Load in GUI and compare for few some videos
      - Preds are pretty similar

--- emotion/core/utils/utils.py
+++ emotion/core/utils/utils.py
@@ -21,8 +21,8 @@ VID_EXTS += [fmt.upper() for fmt in VID_EXTS]
 # These are the files to use for emotional curve prediction
 # VIZ_SENT_PRED_FN = 'sent_biclass.csv' if os.path.abspath('.').startswith('/Users/eric') else 'sent_biclass_19.csv'
 VIZ_SENT_PRED_FN = 'sent_biclass_19.csv'
-# AUDIO_SENT_PRED_FN = 'audio-valence_reg_4.csv'
-AUDIO_SENT_PRED_FN = 'audio-valence_class_2.csv'
+AUDIO_SENT_PRED_FN = 'audio-valence_class_70.csv'
+# AUDIO_SENT_PRED_FN = 'audio-valence_class_69.csv'
 
 
 # CMU Movie Summary path
